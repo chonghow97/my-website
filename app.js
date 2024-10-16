b10b542713be9ec367221e67260c7b1a048f36aa
chonghow97 <admin@chonghow.com>
2024-10-16 20:59:06 +0800

    Lesson 10: working

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const morgan = require("morgan");
 const mongoose = require("mongoose");
 const Blog = require("./models/blog");
 
+// express app
+const app = express();
+
 // connect to mongodb
 const dbURI = "mongodb+srv://xfer:1030@chongtuts.gezlv.mongodb.net/blogDB";
 mongoose
@@ -14,9 +17,6 @@ mongoose
   })
   .catch((err) => console.log(err));
 
-// express app
-const app = express();
-
 // register view engine
 app.set("view engine", "ejs");
 
@@ -24,12 +24,31 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
+
 // routes
 app.get("/", (req, res) => {
   res.redirect("blogs");
 });
 
 // blog routes
+// Reorder routes to handle "create" route first
+app.get("/blogs/create", (req, res) => {
+  res.render("create", {
+    title: "Create a new Blog",
+  });
+});
+
+app.get("/blogs/:id", (req, res) => {
+  const id = req.params.id;
+  Blog.findById(id)
+    .then((result) => {
+      res.render("details", { blog: result, title: "Blog Details" });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
 app.get("/blogs", (req, res) => {
   Blog.find()
     .sort({ createdAt: -1 })
@@ -45,7 +64,6 @@ app.get("/blogs", (req, res) => {
 });
 
 app.post("/blogs", (req, res) => {
-  // console.log(req.body);
   const blog = new Blog(req.body);
 
   blog
@@ -79,6 +97,18 @@ app.get("/blogs/create", (req, res) => {
   res.render("create", {
     title: "Create a new Blog",
   });
+});
+
+app.delete("/blogs/:id", (req, res) => {
+  const id = req.params.id;
+
+  Blog.findByIdAndDelete(id)
+    .then((result) => {
+      res.json({ redirect: "/blogs" });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
 // 404 page
